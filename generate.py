77fc353f9950825395d71928662b73fe10dc00c5
Sergei <mikhailovse@mail.ru>
2018-04-07 23:34:54 +0300

    changed names of variable

--- generate.py
+++ generate.py
@@ -4,20 +4,20 @@ import os
 from sys import stdin
 
 
-def uploadmodel(path, d):
+def upload_model(path, model_dict):
     """Создаем словарь с моделью, храним аналогичным образом, как в train.py. Первое слово в строке наш ключ, потом
        через пробел пара val_chis что значит что слово val встречалось после слова key  chis раз. Разделяем функцией
        split по _"""
     f = open(path)
-    for x in f:
-        x = x.split()
-        key = x[0]
-        for i in range(1, len(x)):
-            symb = x[i].split('_')
-            if not d.get(key):
-                d[key] = {symb[0]: symb[1]}
+    for current_string in f:
+        current_string = current_string.split()
+        key = current_string[0]
+        for i in range(1, len(current_string)):
+            values = current_string[i].split('_')
+            if not model_dict.get(key):
+                model_dict[key] = {values[0]: values[1]}
             else:
-                d[key][symb[0]] = symb[1]
+                model_dict[key][values[0]] = values[1]
 
 
 
@@ -26,13 +26,15 @@ def generate(d, current, lenght, output):
     которые могут идти после него в тексте. Далее функцией random.choise выбираеся следующее слово и сразу выводится"""
     if output != 'stdout':
         f = open(output, "w")
-    print(current, ' ', end='')
+        f.write(current + ' ')
+    else:
+        print(current, ' ', end='')
     for i in range(1, lenght):
-        lst = []
-        for x in d[current]:
-            for j in range(int(d[current][x])):
-                lst.append(x)
-        current = random.choice(lst)
+        generation_list = []
+        for value in d[current]:
+            for j in range(int(d[current][value])):
+                generation_list.append(value)
+        current = random.choice(generation_list)
         if(output == 'stdout'):
             print(current, ' ', end='')
         else:
@@ -45,13 +47,13 @@ parser.add_argument('--model', dest='model', required=True, help='path to file w
 parser.add_argument('--output', dest='output', default='stdout', help='path to output file')
 parser.add_argument('--seed', dest='seed', help='it is seed')
 
-dict= {}
+model_dict = {}
 """Вызываем функцию которая загружает модель, далее проверяем задано ли первое слово, если нет то выбираем его случайным
    образом среди всех ключей словаря, записываем в current. Вызываем функцию генератора текста"""
-uploadmodel(parser.parse_args().model, dict)
+upload_model(parser.parse_args().model, model_dict)
 if parser.parse_args().seed:
     current = parser.parse_args().seed
 else:
     """if no seed then random"""
-    current = random.choice(list(dict.keys()))
-generate(dict, current, parser.parse_args().lenght, parser.parse_args().output)
+    current = random.choice(list(model_dict.keys()))
+generate(model_dict, current, parser.parse_args().lenght, parser.parse_args().output)
--- train.py
+++ train.py
@@ -4,21 +4,21 @@ import os
 from sys import stdin
 
 
-def func(str, lc, d):
-    if lc:
-        str = str.lower()
-    words = re.findall(r'\w+', str)
+def create_model(current_str, islow, model_dict):
+    if islow:
+        current_str = current_str.lower()
+    words = re.findall(r'\w+', current_str)
     """Парсим строку, оставляем только буквы, приводим к нижнему регистру если нужно"""
     for x in range(0, len(words) - 1):
         """Составляем словарь с моделью, для каждого слова свой подсловарь в котором частота вхождений других слов после 
            текущего. Для каждого слова смотрим есть ли в словаре такой ключ, есть ли значения у ключа, для каждого 
            случая выполняем необходимые действия"""
-        if not d.get(words[x]):
-            d[words[x]] = {words[x+1]: 1}
-        elif not d[words[x]].get(words[x+1]):
-            d[words[x]][words[x+1]] = 1
+        if not model_dict.get(words[x]):
+            model_dict[words[x]] = {words[x+1]: 1}
+        elif not model_dict[words[x]].get(words[x+1]):
+            model_dict[words[x]][words[x+1]] = 1
         else:
-            d[words[x]][words[x + 1]] += 1
+            model_dict[words[x]][words[x + 1]] += 1
     """Возвращаем последнее слово строки, чтобы связать его с первым словом следующей строки"""
     return words[len(words) - 1]
 
@@ -28,30 +28,30 @@ parser.add_argument('--input-dir', dest='dir', default='stdin', help='directory 
 parser.add_argument('--model', dest='model',required=True, help='path to file with model')
 parser.add_argument('--lc', action = 'store_true', help='is lowercase?')
 
-dict = {}
+model_dict = {}
 if parser.parse_args().dir == 'stdin':
-    tmp = ''
-    for s in stdin:
-        tmp = tmp + ' ' + s
+    new_string = ''
+    for current_string in stdin:
+        new_string = ' ' + current_string
         """Добавляем в начало строки последнее слово предыдущей, которое получили на прошлом шаге цикла если 
            цикл выполняется в первый раз, то добавится пробел, которые потом все равно обрежется. Вызываем функцию"""
-        tmp = func(tmp, parser.parse_args().lc, dict)
+        new_string = create_model(new_string, parser.parse_args().lc, model_dict)
 else:
     for file in os.listdir(path=parser.parse_args().dir):
         """Перебираем файлы в директории, если это текстовый файл, то открываем его и построчно считываем"""
         if file[-4:] == ".txt":
             f = open(parser.parse_args().dir + '\\' + file)
-            tmp = ''
-            for s in f:
-                tmp = tmp + ' ' + s
-                tmp = func(tmp, parser.parse_args().lc, dict)
+            new_string = ''
+            for current_string in f:
+                new_string += ' ' + current_string
+                new_string = create_model(new_string, parser.parse_args().lc, model_dict)
             f.close()
 f = open(parser.parse_args().model, "w")
-for x in dict:
+for x in model_dict:
     f.write(x + " ")
-    for y in dict[x]:
+    for y in model_dict[x]:
         """Записываем в файл модель. Для каждого слова key идет строка вида key val_chis где val это слово которое
            встречалась после слова key, а chis это количество встречаний"""
-        f.write(y + "_" + str(dict[x][y]) + " ")
+        f.write(y + "_" + str(model_dict[x][y]) + " ")
     f.write("\n")
 f.close()
